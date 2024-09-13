7895b24be3d6fa6cad7792c6cd4996b21cd431cb
AnTran32 <anxiktrai222@gmail.com>
2024-09-13 13:52:16 +0700

    commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,58 +3,73 @@ import './Sidebar.scss';
 import { toggleMenu } from './helpers';
 
 function Sidebar() {
-  const [isOpenTask, setIsOpen] = useState(false);
- 
+  const [isOpenTask, setIsOpenTask] = useState(false);
+  const [isOpenSidebar, setIsOpenSidebar] = useState(true); 
+
   return (
-    <div className="sidebar">
-      <div className="sidebar__Account">Acc
-        <div className="sidebar__Account-menu">
-          <i class="fa-solid fa-bars"></i>
+    <>
+      {!isOpenSidebar && (
+        <div className="sidebar-toggle" onClick={() => setIsOpenSidebar(true)}>
+          <i className="fa-solid fa-bars"></i>
         </div>
-      </div>
-      <div className="sidebar__list">
-        <div className="sidebar__item">
-          <a className="sidebar__item-text">
-            <span className="sidebar__item-text-icon">
-              <i className="fa-solid fa-circle-plus"></i>
-            </span>
-            Thêm nhiệm vụ
-          </a>
+      )}
+     
+      <div className={`sidebar ${isOpenSidebar ? 'open' : ''}`}>
+        <div className="sidebar__Account">Account
+          <div className="sidebar__Account-menu" onClick={() => setIsOpenSidebar(false)}>
+            <i className="fa-solid fa-bars"></i> 
+          </div>
         </div>
-        <div className="sidebar__item">
-          <a className="sidebar__item-text">
-            <span className="sidebar__item-text-icon">
-              <i className="fa-solid fa-house"></i>
-            </span>
-            Trang chủ
-          </a>
-        </div>
-        <div className={"sidebar__item ${isOpenTask ? 'open' : ''}"} onClick={() => toggleMenu(isOpenTask, setIsOpen)}>
-          <a className="sidebar__item-text" >
-            <span className="sidebar__item-text-icon">
-              <i className="fa-solid fa-list-check"></i>
-            </span>
-            Công việc
-          </a>
-          
-        </div>
-        {isOpenTask && (
+        <div className="sidebar__list">
+          <div className="sidebar__item">
+            <a className="sidebar__item-text">
+              <span className="sidebar__item-text-icon">
+                <i className="fa-solid fa-circle-plus"></i>
+              </span>
+              Thêm nhiệm vụ
+            </a>
+          </div>
+          <div className="sidebar__item">
+            <a className="sidebar__item-text">
+              <span className="sidebar__item-text-icon">
+                <i className="fa-solid fa-house"></i>
+              </span>
+              Trang chủ
+            </a>
+          </div>
+          <div className="sidebar__item" onClick={() => toggleMenu(isOpenTask, setIsOpenTask)}>
+            <a className="sidebar__item-text">
+              <span className="sidebar__item-text-icon">
+                <i className="fa-solid fa-list-check"></i>
+              </span>
+              Công việc
+            </a>
+          </div>
+          {isOpenTask && (
             <div className="sidebar__list-dropdownMenu">
-              <td className="sidebar__list-dropdownMenu-item">
-                <span className='sidebar__list-dropdownMenu-item-icon'><i class="fa-solid fa-bullseye"></i></span>
-                project 1
-              </td> 
-                <br></br>
-              <td className="sidebar__list-dropdownMenu-item">
-              <span className='sidebar__list-dropdownMenu-item-icon'><i class="fa-solid fa-bullseye"></i></span>
-                project 2</td> <br></br>
-              <td className="sidebar__list-dropdownMenu-item">
-              <span className='sidebar__list-dropdownMenu-item-icon'><i class="fa-solid fa-bullseye"></i></span>
-                project 3</td>
+              <div className="sidebar__list-dropdownMenu-item">
+                <span className="sidebar__list-dropdownMenu-item-icon">
+                  <i className="fa-solid fa-bullseye"></i>
+                </span>
+                Project 1
+              </div>
+              <div className="sidebar__list-dropdownMenu-item">
+                <span className="sidebar__list-dropdownMenu-item-icon">
+                  <i className="fa-solid fa-bullseye"></i>
+                </span>
+                Project 2
+              </div>
+              <div className="sidebar__list-dropdownMenu-item">
+                <span className="sidebar__list-dropdownMenu-item-icon">
+                  <i className="fa-solid fa-bullseye"></i>
+                </span>
+                Project 3
+              </div>
             </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
