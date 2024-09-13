db052e609369b916ac1eb86afb8ca2009b7b87e0
AnTran32 <anxiktrai222@gmail.com>
2024-09-13 08:04:15 +0700

    second commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,21 +1,60 @@
-import "./Sidebar.scss"
+import React, { useState } from 'react';
+import './Sidebar.scss';
 
 function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false); 
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); 
+  };
+
   return (
-    <>
-      <div className="sidebar">
-        <h3 className="sidebar__logo">LOGO</h3>
-        <div className="sidebar__list">
-          <div className="sidebar__item">
-            <a className="sidebar__item-text">Trang chủ</a>
-          </div>
-          <div className="sidebar__item">
-            <a className="sidebar__item-text">Công việc</a>
-          </div>
+    <div className="sidebar">
+      <div className="sidebar__Account">Acc</div>
+      <div className="sidebar__list">
+        <div className="sidebar__item">
+          <a className="sidebar__item-text">
+            <span className="sidebar__item-text-icon">
+              <i className="fa-solid fa-circle-plus"></i>
+            </span>
+            Thêm nhiệm vụ
+          </a>
         </div>
+        <div className="sidebar__item">
+          <a className="sidebar__item-text">
+            <span className="sidebar__item-text-icon">
+              <i className="fa-solid fa-house"></i>
+            </span>
+            Trang chủ
+          </a>
+        </div>
+        <div className="sidebar__item" onClick={toggleMenu}>
+          <a className="sidebar__item-text" >
+            <span className="sidebar__item-text-icon">
+              <i className="fa-solid fa-list-check"></i>
+            </span>
+            Công việc
+          </a>
+          
+        </div>
+        {isOpen && (
+            <div className="sidebar__list-dropdownMenu">
+              <td className="sidebar__list-dropdownMenu-item">
+                <span className='sidebar__list-dropdownMenu-item-icon'><i class="fa-solid fa-bullseye"></i></span>
+                project 1
+              </td> 
+                <br></br>
+              <td className="sidebar__list-dropdownMenu-item">
+              <span className='sidebar__list-dropdownMenu-item-icon'><i class="fa-solid fa-bullseye"></i></span>
+                project 2</td> <br></br>
+              <td className="sidebar__list-dropdownMenu-item">
+              <span className='sidebar__list-dropdownMenu-item-icon'><i class="fa-solid fa-bullseye"></i></span>
+                project 3</td>
+            </div>
+          )}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,52 +1,58 @@
-import { useEffect, useState } from "react"
-import "./Task.scss"
+import { useEffect, useState } from "react";
+import "./Task.scss";
+
 function Task() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/v1/tasks")
       .then((response) => response.json())
-      .then((data) => setData(data))
-  }, [])
+      .then((data) => setData(data));
+  }, []);
 
-  
+  console.log(data);
 
-  console.log(data)
   return (
     <>
-      <table className="task">
-        <thead>
-          <tr className="task__row">
-            <th> <button className="task__row-buttonSelectAll">Select all</button></th>
-            <th>Số thứ tự</th>
-            <th>Tên công việc</th>
-            <th>Trạng thái</th>
-            <th>Deadline</th>
-            <th>Hành động</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => {
-            return (
-              <tr className="task__item clo">
-                <td>
-                  <input type="checkbox" />
-                </td>
-                <td>{index + 1}</td>
-                <td>{item.title}</td>
-                <td>{item.status}</td>
-                <td>20/11/2022</td>
-                <td>
-                  <button className="task__item-buttonEdit">Sửa</button>
-                  <button className="task__item-buttonDelete">Xóa</button>
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <div className="page">Công việc / </div>
+      <div className="task-container">
+      <div className="task__header">
+        <div className="task__header-name">Tên project</div>
+        <div className="task__header-items">
+          <span className="task__header-items-item">Lead:</span> Back End
+                <br />
+          <span className="task__header-items-item">Ngày khởi tạo:</span> 22/12/2024
+        </div>
+      </div>
+      <div className="task">
+        {data.map((item) => (
+          <div className="task__item" >
+            <div className="task__item-content">
+              <div className="task__item-main">
+                <input type="checkbox" />
+                <span className="task__item-main-text">{item.title}</span>
+              </div>
+              <div className="task__item-info">
+                Status: {item.status}
+                <br />
+                Deadline: 22/12/2024
+              </div>
+            </div>
+            <div className="task__item-actions">
+              <button className="task__item-actions-buttonEdit">
+                <i className="fa-solid fa-pen"></i>
+              </button>
+              <button className="task__item-actions-buttonDelete">
+                <i className="fa-solid fa-trash"></i>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
     </>
-  )
+    
+  );
 }
 
-export default Task
+export default Task;
